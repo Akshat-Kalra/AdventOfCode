b160e8b92d4703e41f96759ea725f88f98ebce95
akshatkalra <akshatkalra2005@gmail.com>
2024-12-03 02:52:00 -0800

    Completed Day 3 Part 1

--- Day 3/solution.py
+++ Day 3/solution.py
@@ -1,1 +1,17 @@
-test_string = "fkjegnkjrngkjnmul(2,5)ffsnsfsffs"
+#Part 1
+import re
+
+with open('/Users/akshatkalra/Desktop/AdventOfCode/Day 3/data.txt', 'r') as file:
+    data_string = file.read()
+
+
+parsed = re.findall(r'mul\([0-9]?[0-9]?[0-9],[0-9]?[0-9]?[0-9]\)', data_string)
+
+# print (parsed)
+
+result = 0
+for mul in parsed:
+    to_multiply = re.findall(r'[0-9]?[0-9]?[0-9]', mul)
+    result += int(to_multiply[0]) * int(to_multiply[1])
+    
+print(result)
